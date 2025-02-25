c2a85a1e8acd9e55ccb8aaa42e7aca3b5e64c267
Ritaban Chaudhuri <inrcr27@gmail.com>
2025-02-25 21:47:59 +0530

    add box2d

--- src/Physics.cpp
+++ src/Physics.cpp
@@ -1,1 +1,13 @@
 #include "Physics.h"
+
+b2World Physics::world{};
+
+void Physics::Init()
+{
+
+}
+
+void Physics::Update(float deltaTime)
+{
+
+}
--- src/Physics.h
+++ src/Physics.h
@@ -1,6 +1,13 @@
 #pragma once
-#include 
+#include "vendor/box2d/src/world.h" 
+
 class Physics
 {
+public:
+	static void Init();
+	static void Update(float deltaTime);
+private:
+	static b2World world;
+
 };
 
