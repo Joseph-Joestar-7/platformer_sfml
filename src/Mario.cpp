85ad74c30cf83c6813e43a98c8e5e94c91b7afe0
Ritaban Chaudhuri <inrcr27@gmail.com>
2025-03-02 20:16:55 +0530

    lock rotation

--- src/Mario.cpp
+++ src/Mario.cpp
@@ -7,12 +7,13 @@ void Mario::Begin()
 	b2BodyDef bodyDef = b2DefaultBodyDef();
 	bodyDef.position = { position.x, position.y};
 	bodyDef.type = b2_dynamicBody;
+	bodyDef.fixedRotation = true;
 	bodyId = b2CreateBody(Physics::worldId, &bodyDef);
 	b2Polygon shape{};
 	shape = b2MakeBox(0.5f,0.75f);
 	b2ShapeDef shapeDef = b2DefaultShapeDef();
 	shapeDef.density = 1.0f;
-	shapeDef.friction = 1.0f;
+	shapeDef.friction = 0.3f;
 
 	b2CreatePolygonShape(bodyId, &shapeDef, &shape);
 }
@@ -25,22 +26,20 @@ void Mario::Update(float deltaTime)
 	{
 		b2Body_ApplyForceToCenter(bodyId, b2Vec2(move, 0.0f), true);
 	}
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 	{
 		b2Body_ApplyForceToCenter(bodyId, b2Vec2(-move, 0.0f), true);
 	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		b2Body_ApplyForceToCenter(bodyId, b2Vec2(0.0f,-move), true);
+	}
+
 
 	position = sf::Vector2f(b2Body_GetPosition(bodyId).x, b2Body_GetPosition(bodyId).y);
 	angle = b2Rot_GetAngle(b2Body_GetRotation(bodyId)) * (180.0f/M_PI);
 
-	/*if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-		position.x += move * deltaTime;
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-		position.x -= move * deltaTime;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-		position.y += move * deltaTime;
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-		position.y -= move * deltaTime;*/
+	
 
 }
 
