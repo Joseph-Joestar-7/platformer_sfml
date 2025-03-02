35ebfa5bd31ce747ade685ef60a4a9dd1664ab9d
Ritaban Chaudhuri <inrcr27@gmail.com>
2025-03-02 17:36:16 +0530

    make rendertarget public

--- src/Renderer.h
+++ src/Renderer.h
@@ -8,8 +8,8 @@ public:
 	void Draw( 
 		const sf::Texture& texture, const sf::Vector2f& position,
 		const sf::Vector2f& size,float angle =0.0f);
+	sf::RenderTarget& target;
 private:
 	sf::Sprite sprite{};
-	sf::RenderTarget& target;
 };
 
